69ea6c2b28d525e0f67716950fcbe859eeda18ba
Roman Brito <romanbrito1@gmail.com>
2017-06-25 17:24:23 -0500

    second commit

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
+
+import Pic from './Pic';
 
 export default class App extends Component {
 
   render() {
     return (
-      <MuiThemeProvider>
-        <RaisedButton
-          label="Oh Yeah we did it"
-          onTouchTap={() => {
-            console.log('it works')
-          }}
-        />
-      </MuiThemeProvider>
+      <div>
+        <MuiThemeProvider>
+          <RaisedButton
+            label="Oh Yeah we did it"
+            onTouchTap={() => {
+              console.log('it works')
+            }}
+          />
+        </MuiThemeProvider>
+
+        {/*Display pics from instagram*/}
+        <Pic/>
+
+      </div>
     );
   }
 }
---
+++ imports/ui/Pic.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class Pic extends Component {
+
+  render() {
+    return(
+      <h1>Pic component</h1>
+    );
+  }
+}
